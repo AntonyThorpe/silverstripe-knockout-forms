1e4135c57f98febf15c3bb81599a48185a166d9a
Antony Thorpe <1023740+AntonyThorpe@users.noreply.github.com>
2024-03-06 08:01:23 +1300

    Update knockoutforms.js
    Improve formatting

--- client/javascript/knockoutforms.js
+++ client/javascript/knockoutforms.js
@@ -1,49 +1,81 @@
-;
-(function(factory) {
-	//CommonJS
-	if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
-		factory(require("knockout"), exports);
-		//AMD
-	} else if (typeof define === "function" && define.amd) {
-		define(["knockout", "exports"], factory);
-		//normal script tag
-	} else {
-		factory(ko);
-	}
-}(function(ko) {
+(function (factory) {
+    //CommonJS
+    if (
+        typeof require === "function" &&
+        typeof exports === "object" &&
+        typeof module === "object"
+    ) {
+        factory(require("knockout"), exports);
+        //AMD
+    } else if (typeof define === "function" && define.amd) {
+        define(["knockout", "exports"], factory);
+        //normal script tag
+    } else {
+        factory(ko);
+    }
+})(function (ko) {
+    "use strict";
 
-	"use strict";
+    /**
+     * setKnockout Binding Handler
+     *
+     * For passing PHP variables and the observable to Knockout
+     * @param HTML element The element the binding handler is attached to
+     * @param function valueAccessor The value, wrapped in a function, passed from the PHP function into the Knockout observable.
+     * @param function allBindings Other binding handlers on the element
+     * @example {value:'The Enterprise', observable:'spaceship'}
+     */
+    ko.bindingHandlers.setKnockout = {
+        init: function (element, valueAccessor, allBindings) {
+            // Definitions
+            var unwrapValueAccessor = ko.unwrap(valueAccessor());
+            var observable;
 
-	/**
-	 * setKnockout Binding Handler
-	 *
-	 * For passing PHP variables and the observable to Knockout
-	 * @param HTML element The element the binding handler is attached to
-	 * @param function valueAccessor The value, wrapped in a function, passed from the PHP function into the Knockout observable.
-	 * @param function allBindings Other binding handlers on the element
-	 * @example {value:'The Enterprise', observable:'spaceship'}
-	 */
-	ko.bindingHandlers.setKnockout = {
-		init: function(element, valueAccessor, allBindings) {
-			// Definitions
-			var unwrapValueAccessor = ko.unwrap(valueAccessor());
-			var observable;
+            // obtain the observable from either this binding (needed with a custom binding) or one of Knockouts standard bindings
+            if (unwrapValueAccessor.observable) {
+                //
+                observable = unwrapValueAccessor.observable;
+            } else {
+                // obtain the observable key from the allBindings Accessor
+                var key = ko.utils.arrayFirst(
+                    [
+                        "visible",
+                        "hidden",
+                        "text",
+                        "html",
+                        "class",
+                        "css",
+                        "style",
+                        "attr",
+                        "if",
+                        "ifnot",
+                        "with",
+                        "using",
+                        "let",
+                        "component",
+                        "click",
+                        "event",
+                        "submit",
+                        "enable",
+                        "disable",
+                        "value",
+                        "textInput",
+                        "hasFocus",
+                        "checked",
+                        "options",
+                        "selectedOptions",
+                    ],
+                    function (item) {
+                        return allBindings.has(item);
+                    }
+                );
+                observable = allBindings.get(key);
+            }
 
-			// obtain the observable from either this binding (needed with a custom binding) or one of Knockouts standard bindings
-			if (unwrapValueAccessor.observable) { //
-				observable = unwrapValueAccessor.observable;
-			} else {
-				// obtain the observable key from the allBindings Accessor
-				var key = ko.utils.arrayFirst(["visible", "hidden", "text", "html", "class", "css", "style", "attr", "if", "ifnot", "with", "using", "let", "component", "click", "event", "submit", "enable", "disable", "value", "textInput", "hasFocus", "checked", "options", "selectedOptions"], function(item) {
-					return allBindings.has(item);
-				});
-				observable = allBindings.get(key);
-			}
-
-			// Set the Observable value
-			if (unwrapValueAccessor.value) {
-				observable(unwrapValueAccessor.value);
-			}
-		}
-	};
-})); // close module loader
+            // Set the Observable value
+            if (unwrapValueAccessor.value) {
+                observable(unwrapValueAccessor.value);
+            }
+        },
+    };
+}); // close module loader
